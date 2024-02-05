396f041e285e6844f82839191a046fda39c94e0a
Vladislav676 <vladnety134@gmail.com>
2024-02-05 20:44:22 +0300

    удалены лишние файлы

--- TelegramBot/Main.js
+++ TelegramBot/Main.js
@@ -1,2 +1,47 @@
 const { Telegraf } = require('telegraf');
-const 
+const axios = require('axios');
+
+const bot = new Telegraf('TOKEN');
+
+// Обработка команды /start
+bot.start((ctx) => {
+  ctx.reply('Привет! Я твой телеграм-бот для HTTP-запросов.');
+});
+
+// Обработка текстовых сообщений
+bot.on('text', async (ctx) => {
+  const command = ctx.message.text;
+
+  if (command.startsWith('/get') || command.startsWith('/post') || command.startsWith('/put') || command.startsWith('/delete')) {
+    const [method, url] = command.split(' ');
+
+    try {
+      let response;
+      if (method === '/get') {
+        response = await axios.get(url);
+      } else if (method === '/post') {
+        response = await axios.post(url);
+      } else if (method === '/put') {
+        response = await axios.put(url);
+      } else if (method === '/delete') {
+        response = await axios.delete(url);
+      }
+
+      ctx.reply(JSON.stringify(response.data, null, 2));
+    } catch (error) {
+      ctx.reply(`Произошла ошибка: ${error.message}`);
+    }
+  } else {
+    ctx.reply('Неправильный формат команды. Используйте /get, /post, /put или /delete, а затем URL.');
+  }
+});
+
+// Обработка команды /help
+bot.help((ctx) => {
+  ctx.reply('Используйте команды /get, /post, /put или /delete, а затем укажите URL для выполнения соответствующего HTTP-запроса.');
+});
+
+// Запуск бота
+bot.launch().then(() => {
+  console.log('Бот запущен');
+});
--- TelegramBot/secretsData.js
+++
@@ -1,1 +0,0 @@
-export default token = "6710300650:AAHGRLpH428-Eic-lAD45Z1J9NIXCwvjfIw";
